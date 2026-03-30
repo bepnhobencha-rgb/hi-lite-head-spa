2931cbef3a60101edf67b75a149455fc1947178d
Johny Tran <hungnguyenbcca@gmail.com>
2026-03-30 08:54:31 +0000

    File changes

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import ScrollToTop from "./ScrollToTop";
 
 export default function Layout() {
   return (
     <div className="min-h-screen flex flex-col font-body">
+      <ScrollToTop />
       <Navbar />
       <main className="flex-1">
         <Outlet />
